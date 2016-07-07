f58cfa9e854bda63f6ad6557592d8d55eb47efa6
Dave @ DevedUp <email@devedup.com>
2016-07-07 20:37:40 +0100

    fixed deprecations

--- OHAttributedLabel/Source/OHAttributedLabel.m
+++ OHAttributedLabel/Source/OHAttributedLabel.m
@@ -755,7 +755,7 @@ NSDataDetector* sharedReusableDataDetector(NSTextCheckingTypes types)
 	[super setTextColor:color]; // will call setNeedsDisplay too
 }
 
--(void)setTextAlignment:(UITextAlignment)alignment
+-(void)setTextAlignment:(NSTextAlignment)alignment
 {
     if (_attributedText)
     {
@@ -769,7 +769,7 @@ NSDataDetector* sharedReusableDataDetector(NSTextCheckingTypes types)
 	[super setTextAlignment:alignment]; // will call setNeedsDisplay too
 }
 
--(void)setLineBreakMode:(UILineBreakMode)lineBreakMode
+-(void)setLineBreakMode:(NSLineBreakMode)lineBreakMode
 {
     if (_attributedText)
     {
